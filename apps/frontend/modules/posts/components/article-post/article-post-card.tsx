69ba74dfa591e5946bbafc9d258bfc0a83d37d6f
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-09-20 12:28:53 -0700

    Update article-post-card.tsx

--- apps/frontend/modules/posts/components/article-post/article-post-card.tsx
+++ apps/frontend/modules/posts/components/article-post/article-post-card.tsx
@@ -1,8 +1,8 @@
 import type { ArticlePostAggregate } from "@modules/refound/models/post.aggregate";
 import NextImage from "next/image";
 import { trimText } from "@utils/trim-text";
-import { AccountBadge } from "../image-post/account-badge";
 import { InteractionsBadge } from "../image-post/interactions-badge";
+import { AccountBadge } from "../image-post/account-badge";
 
 export const ArticlePostCard = ({
 	articlePost: { title, body, postId, coverImage, createdAt, creator, tags, interactions },
@@ -10,7 +10,7 @@ export const ArticlePostCard = ({
 	articlePost: ArticlePostAggregate;
 }) => {
 	return (
-		<article className="group">
+		<article className="flex flex-col gap-2 mb-8 group">
 			<div className="relative">
 				{coverImage ? (
 					<figure className="relative w-full pb-[80%] overflow-hidden rounded-md">
@@ -31,24 +31,26 @@ export const ArticlePostCard = ({
 									</span>
 								))}
 							</div>
-							<div className="translate-y-[100%] duration-150 group-hover:translate-y-0 p-4 text-white text-sm">
-								<time
-									className="text-xs"
-									dateTime={new Date(createdAt).toISOString()}
-								>
-									{new Date(createdAt).toDateString()}
-								</time>
+							<div className="translate-y-[100%] duration-150 group-hover:translate-y-0 p-2 text-white text-sm">
+								<div className="flex flex-row justify-between w-full mt-2">
+									<InteractionsBadge interactionList={interactions} />
+									<time
+										className="text-xs"
+										dateTime={new Date(createdAt).toISOString()}
+									>
+										{new Date(createdAt).toDateString()}
+									</time>
+								</div>
 							</div>
 						</figcaption>
 					</figure>
 				) : (
-					<div className="relative w-full pb-[80%] overflow-hidden rounded-md bg-zinc-200">
+					<div className="relative w-full pb-[80%] overflow-hidden rounded-md bg-slate-200">
 						<div
-							className="absolute top-0 bottom-0 left-0 right-0 p-4 overflow-hidden prose-sm prose"
+							className="absolute top-0 bottom-0 left-0 right-0 w-full h-full px-4 pt-4 overflow-hidden prose-sm prose"
 							dangerouslySetInnerHTML={{ __html: body }}
 						/>
-
-						<div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-between prose-sm prose transition-opacity duration-300 opacity-0 bg-gradient-to-b from-transparent to-primary/90 group-hover:opacity-100">
+						<div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-between transition-opacity duration-300 opacity-0 bg-gradient-to-b from-transparent to-primary/90 group-hover:opacity-100">
 							<div className="w-full p-4">
 								{tags.map((tag) => (
 									<span
@@ -59,14 +61,16 @@ export const ArticlePostCard = ({
 									</span>
 								))}
 							</div>
-
-							<div className="translate-y-[100%] duration-150 group-hover:translate-y-0 p-4 text-white text-sm">
-								<time
-									className="text-xs"
-									dateTime={new Date(createdAt).toISOString()}
-								>
-									{new Date(createdAt).toDateString()}
-								</time>
+							<div className="translate-y-[100%] duration-150 group-hover:translate-y-0 p-2 text-white text-sm">
+								<div className="flex flex-row justify-between w-full mt-2">
+									<InteractionsBadge interactionList={interactions} />
+									<time
+										className="text-xs"
+										dateTime={new Date(createdAt).toISOString()}
+									>
+										{new Date(createdAt).toDateString()}
+									</time>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -77,11 +81,10 @@ export const ArticlePostCard = ({
 					aria-label="go to article"
 				/>
 			</div>
-			<div className="flex flex-row justify-between w-full px-2 mt-2 mb-8">
+			<div className="flex flex-row items-baseline justify-between w-full pl-2">
 				<h1 className="text-sm font-bold">{trimText(title, 90)}</h1>
 				<AccountBadge profile={creator} />
 			</div>
-			<InteractionsBadge interactionList={interactions} />
 		</article>
 	);
 };
