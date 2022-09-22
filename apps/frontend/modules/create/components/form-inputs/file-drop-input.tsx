21a7ca7a3639029b7390707077fed6cfcde97539
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-09-22 01:07:23 -0700

    better dropzone color

--- apps/frontend/modules/create/components/form-inputs/file-drop-input.tsx
+++ apps/frontend/modules/create/components/form-inputs/file-drop-input.tsx
@@ -168,7 +168,7 @@ export const FileDropInput = ({
 				onDragLeave={handleDragLeave}
 				onClick={handleClick}
 				className={`relative flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 overflow-hidden ${
-					state.inDropZone ? "bg-red-600" : ""
+					state.inDropZone ? "bg-green-200" : ""
 				}`}
 			>
 				<div className="flex flex-col items-center justify-center pt-5 pb-6">
